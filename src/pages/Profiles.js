e8b3393d0a4ee1bb3892c7d3d47c355b746ee61f
Akanimo Sam Okure <akan.okure@gmail.com>
2023-07-21 23:20:48 +0100

    reuse components

--- src/pages/Profiles.js
+++ src/pages/Profiles.js
@@ -1,60 +1,60 @@
 import { useState, useEffect } from "react";
+import List from "../components/List";
+import Link from "../components/Link";
+import "./Profile.css";
+
 function Profile({ userName }) {
   const [loading, setLoading] = useState(false);
   const [profile, setProfile] = useState({});
+
   useEffect(() => {
     async function fetchData() {
-      const profile = await fetch("https://api.github.com/users/${userName}");
+      const profile = await fetch(`https://api.github.com/users/${userName}`);
       const result = await profile.json();
+
       if (result) {
         setProfile(result);
         setLoading(false);
       }
     }
+
     fetchData();
   }, [userName]);
+
+  const items = [
+    {
+      field: "html_url",
+      value: <Link url={profile.html_url} title={profile.html_url} />,
+    },
+    {
+      field: "repos_url",
+      value: <Link url={profile.repos_url} title={profile.repos_url} />,
+    },
+    { field: "name", value: profile.name },
+    { field: "company", value: profile.company },
+    { field: "location", value: profile.location },
+    { field: "email", value: profile.email },
+    { field: "bio", value: profile.bio },
+  ];
+
   return (
-    <div>
-            <h2>About me</h2>
-            
+    <div className="Profile-container">
+      <h2>About me</h2>
       {loading ? (
         <span>Loading...</span>
       ) : (
-        <ul>
-          <li>
-            <span>avatar_url: </span>
-            {profile.avatar_url}
-          </li>
-          <li>
-            <span>html_url: </span>
-            {profile.html_url}
-          </li>
-          <li>
-            <span>repos_url: </span>
-            {profile.repos_url}
-          </li>
-          <li>
-            <span>name: </span> {profile.name}
-          </li>
-          <li>
-            <span>company: </span>
-            {profile.company}
-          </li>
-          <li>
-            <span>location: </span>
-            {profile.location}
-          </li>
-          <li>
-            <span>email: </span>
-            {profile.email}
-          </li>
-          <li>
-            <span>bio: </span> {profile.bio}
-          </li>
-        </ul>
+        <div>
+          <img
+            className="Profile-avatar"
+            src={profile.avatar_url}
+            alt={profile.name}
+          />
+
+          <List items={items} />
+        </div>
       )}
-          
     </div>
   );
 }
+
 export default Profile;
